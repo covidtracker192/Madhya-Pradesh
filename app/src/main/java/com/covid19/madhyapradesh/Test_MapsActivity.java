2796fc73b1f98e1d462ac705fb5b721a1e91b5ea
anirudhbelwadi <belwadi.anirudh18@siesgst.ac.in>
2020-06-25 08:47:14 +0530

    Developed, not released

--- app/src/main/java/com/covid19/madhyapradesh/Test_MapsActivity.java
+++ app/src/main/java/com/covid19/madhyapradesh/Test_MapsActivity.java
@@ -53,55 +53,14 @@ public class Test_MapsActivity extends FragmentActivity implements OnMapReadyCal
         mMap.setMyLocationEnabled(true);
 
         // Test Center 1
-        final LatLng center1 = new LatLng(12.959485, 77.574737);
-        mMap.addMarker(new MarkerOptions().position(center1).title("Bangalore Medical College & Research Institute, Bengaluru").snippet("website: http://www.bmcri.org/"));
+        final LatLng center1 = new LatLng(24.115128, 78.668717);
+        mMap.addMarker(new MarkerOptions().position(center1).title("Bundelkhand Medical College &amp; Hospital").snippet("website: http://www.bmcsagar.edu.in/"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(center1));
 
         //Test Center 2
-        final LatLng center2 = new LatLng(12.964658, 77.628138);
-        mMap.addMarker(new MarkerOptions().position(center2).title("Command Hospital (Air Force), Bengaluru").snippet("website: https://www.chaf-b.org/"));
+        final LatLng center2 = new LatLng(24.315521, 78.777772);
+        mMap.addMarker(new MarkerOptions().position(center2).title("Government District Hospital"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(center2));
-
-        //Test Center 3
-        final LatLng center3 = new LatLng(12.937347, 77.590940);
-        mMap.addMarker(new MarkerOptions().position(center3).title("National Institute of Virology, Bangalore Field Unit, Bengaluru"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(center3));
-
-        //Test Center 4
-        final LatLng center4 = new LatLng(12.975883, 77.729613);
-        mMap.addMarker(new MarkerOptions().position(center4).title("Vydehi Institute Of Medical Sciences And Research Centre").snippet("website: http://www.vims.ac.in/"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(center4));
-
-        //Test Center 5
-        final LatLng center5 = new LatLng(12.940737, 77.598334);
-        mMap.addMarker(new MarkerOptions().position(center5).title("National Institute of Mental Health and Neuro Sciences, Bangalore").snippet("website: http://nimhans.ac.in/"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(center5));
-
-        //Test Center 6
-        final LatLng center6 = new LatLng(12.932303, 77.685265);
-        mMap.addMarker(new MarkerOptions().position(center6).title("Sakra World Hospital Lab Services, Devarabeesanahalli Varthur, Hobli").snippet("website: https://www.sakraworldhospital.com/lab-medicine.php"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(center6));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(6));
-
-        //Test Center 7
-        final LatLng center7 = new LatLng(12.896058, 77.5995201);
-        mMap.addMarker(new MarkerOptions().position(center7).title("Apollo Hospitals Bannerghatta").snippet("website: https://bangalore.apollohospitals.com/bannerghattaroad/"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(center7));
-
-        //Test Center 8
-        final LatLng center8 = new LatLng(13.932749, 75.566971);
-        mMap.addMarker(new MarkerOptions().position(center8).title("SHIMOGA Institute of Medical Sciences (SIMS)").snippet("website: https://sims-shimoga.com/"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(center8));
-
-        //Test Center 9
-        final LatLng center9 = new LatLng(12.315320, 76.650410);
-        mMap.addMarker(new MarkerOptions().position(center9).title("Mysore Medical College And Research Institute").snippet("website: https://www.mysoremedicalcollege.com/"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(center9));
-
-        //Test Center 10
-        final LatLng center10 = new LatLng(12.867458, 74.843262);
-        mMap.addMarker(new MarkerOptions().position(center10).title("Wenlock District Hospital, Mangalore"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(center10));
       //  mMap.animateCamera(CameraUpdateFactory.zoomTo(6));
 
 
@@ -110,47 +69,10 @@ public class Test_MapsActivity extends FragmentActivity implements OnMapReadyCal
             @Override
             public void onInfoWindowClick(Marker marker) {
                 if(marker.getPosition().equals(center1)) {
-                    Uri uriUrl = Uri.parse("http://www.bmcri.org/");
+                    Uri uriUrl = Uri.parse("http://www.bmcsagar.edu.in/");
                     Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
                     startActivity(launchBrowser);
                 }
-                else if(marker.getPosition().equals(center2)) {
-                    Uri uriUrl = Uri.parse("https://www.chaf-b.org/");
-                    Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
-                    startActivity(launchBrowser);
-                }
-
-                else if(marker.getPosition().equals(center4)) {
-                    Uri uriUrl = Uri.parse("http://www.vims.ac.in/");
-                    Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
-                    startActivity(launchBrowser);
-                }
-                else if(marker.getPosition().equals(center5)) {
-                    Uri uriUrl = Uri.parse("http://nimhans.ac.in/");
-                    Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
-                    startActivity(launchBrowser);
-                }
-                else if(marker.getPosition().equals(center6)) {
-                    Uri uriUrl = Uri.parse("https://www.sakraworldhospital.com/lab-medicine.php");
-                    Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
-                    startActivity(launchBrowser);
-                }
-                else if(marker.getPosition().equals(center7)) {
-                    Uri uriUrl = Uri.parse("https://bangalore.apollohospitals.com/bannerghattaroad/");
-                    Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
-                    startActivity(launchBrowser);
-                }
-                else if(marker.getPosition().equals(center8)) {
-                    Uri uriUrl = Uri.parse("https://sims-shimoga.com/");
-                    Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
-                    startActivity(launchBrowser);
-                }
-                else if(marker.getPosition().equals(center9)) {
-                    Uri uriUrl = Uri.parse("https://www.mysoremedicalcollege.com/");
-                    Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
-                    startActivity(launchBrowser);
-                }
-
             }
         });
     }
